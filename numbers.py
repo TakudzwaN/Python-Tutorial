1a8d46d32fe3439401dd714c494c69db4fcbd8d0
TakudzwaN <TakudzwaN@users.noreply.github.com>
2019-12-16 23:10:32 +0200

    Day 3 Numbers And Type Conversion In Python 3

---
+++ numbers.py
@@ -0,0 +1,17 @@
+# Float Declaration
+# pi =  3.14159
+# print(pi)
+
+# Adding Numbers 
+# firstNumber = 5
+# secondNumber = 6
+# print(firstNumber + secondNumber)
+
+# User Input And Numbers (Type Conversion). Works with float as well.
+firstNum = input("Please Enter A Number  : ")
+secondNum = input("Please Enter A Number  : ")
+print(int(firstNum) + int(secondNum))
+
+# Int/float to String Conversion
+# daysInFeb = 28
+# print(str(daysInFeb) + " days in February.")
