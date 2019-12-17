e1d59fa6339ad4da5cf60a9665db4231b3f20e1d
TakudzwaN <TakudzwaN@users.noreply.github.com>
2019-12-17 21:49:01 +0200

    Day 4 Get Current Date and Time in Python 3

---
+++ get_current_date.py
@@ -0,0 +1,8 @@
+# Import DateTime Library
+from datetime import datetime
+
+# datetime.now() Function Returns Current Date & Time As An Object
+currentDate = datetime.now()
+
+# Printout Current Date & Time Converted To String Format
+print("Today is " + str(currentDate))
