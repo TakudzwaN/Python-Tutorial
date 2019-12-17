2afd718c36e4c0a285ce370c2a4c2c4675156a04
TakudzwaN <TakudzwaN@users.noreply.github.com>
2019-12-17 23:06:18 +0200

    Day 4 Getting Date From User In Python 3

--- input_date.py
+++ input_date.py
@@ -1,2 +1,13 @@
 # Import DateTime Library
 from datetime import datetime, timedelta
+
+# Get Date From User
+birthday = input("When where you born (day/month/year)? ")
+birthdayDate = datetime.strptime(birthday, "%d/%m/%Y")
+
+# Print User Entered Birthday
+print("Your birthday is " + str(birthdayDate))
+
+oneDay = timedelta(days=1)
+birthEve = birthdayDate - oneDay
+print("Birth Eve is " + str(birthEve))
